b2768d3d271fcb51933715f0e7720bb69347f167
Lucas Valverde <valverdelucas95@gmail.com>
2023-09-23 19:43:51 -0300

    Implementações do Projeto

--- versao_1.js
+++ versao_1.js
@@ -10,3 +10,38 @@
  * 
  * 
  */
+
+console.log("Bem vindo(a) ao jogo de BlackJack 1.0!")
+
+let inicioJogo = confirm("Deseja iniciar uma nova jogada?")
+
+if (inicioJogo) {
+
+   let primeiraCartaUsuario = comprarCarta()
+   let segundaCartaUsuario = comprarCarta()
+   let primeiraCartaBanca = comprarCarta()
+   let segundaCartaBanca = comprarCarta()
+
+   let pontuacaoUsuario = primeiraCartaUsuario.valor + segundaCartaUsuario.valor
+   let pontuacaoBanca = primeiraCartaBanca.valor + segundaCartaBanca.valor
+
+   console.log(`Suas cartas: ${primeiraCartaUsuario.texto} e ${segundaCartaUsuario.texto}. A soma é de: ${pontuacaoUsuario}`)
+   console.log(`Cartas da banca: ${primeiraCartaBanca.texto} e ${segundaCartaBanca.texto}. A soma das cartas da banca é de: ${pontuacaoBanca}`)
+
+   if (pontuacaoUsuario > pontuacaoBanca) {
+      console.log(`Você ganhou com a pontuação de: ${pontuacaoUsuario} pontos contra ${pontuacaoBanca} pontos da banca!`)
+      alert(`Você ganhou com a pontuação de: ${pontuacaoUsuario} pontos contra ${pontuacaoBanca} pontos da banca!\nParabéns!`)
+
+   } else if (pontuacaoBanca > pontuacaoUsuario) {
+      console.log(`A banca ganhou com a pontuação de: ${pontuacaoBanca} pontos contra ${pontuacaoUsuario} pontos do usuário!`)
+      alert(`A banca ganhou com a pontuação de: ${pontuacaoBanca} pontos contra ${pontuacaoUsuario} pontos do usuário!\nMais sorte da próxima vez!`)
+
+   } else if (pontuacaoBanca === pontuacaoUsuario) {
+      console.log("O jogo empatou!")
+      alert("O jogo empatou")
+
+} else {
+      console.log("O jogo acabou!")
+      alert("O jogo acabou!\nAté a próxima!")
+   }
+}
